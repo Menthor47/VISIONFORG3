fc738f29ee34103d8e4105e747c5335e73427416
Menthor47 <menthor88@gmail.com>
2025-08-02 19:27:45 +0300

    FAVICON

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,8 +19,8 @@ export default {
 		},
 		extend: {
 			fontFamily: {
-				'heading': ['Montserrat', 'sans-serif'],
-				'body': ['Merriweather', 'serif'],
+				'heading': ['JetBrains Mono', 'monospace'],
+				'body': ['IBM Plex Sans', 'sans-serif'],
 			},
 			colors: {
 				border: 'hsl(var(--border))',
